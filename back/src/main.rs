e645efa3029766c3af0b4581d7987c4bc1ea789a
Alejandro Alborno <233596652+alej-alborno@users.noreply.github.com>
2025-11-10 20:20:29 +0000

    back: add ratelimit logic

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,40 +1,33 @@
-use axum::{
-    Json, Router,
-    routing::{get, post},
-};
+use axum::{Json, Router, extract::State, routing};
 use serde::{Deserialize, Serialize};
 use sqlx::MySqlPool;
-// use tower_governor::{GovernorLayer, governor::GovernorConfigBuilder};
+use std::{env, net::SocketAddr};
+use tokio::net::TcpListener;
+use tower_governor::{GovernorLayer, governor::GovernorConfig};
 use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    let pool =
-        MySqlPool::connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-            .await
-            .unwrap();
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
+        .await
+        .unwrap();
 
     let app = Router::new()
-        .route("/register", post(register))
-        .route("/health", get(health))
+        .route("/register", routing::post(register))
+        .route("/health", routing::get(health))
         .with_state(pool)
+        .layer(GovernorLayer::new(GovernorConfig::default()))
         .layer(
             CorsLayer::new()
                 .allow_origin(Any)
                 .allow_methods(Any)
                 .allow_headers(Any),
         )
-        /*.layer(GovernorLayer::new(
-            GovernorConfigBuilder::default().finish().unwrap(),
-        ))*/;
+        .into_make_service_with_connect_info::<SocketAddr>();
 
-    let port = std::env::var("PORT").expect("PORT must be set");
-    let listener_ipv4 = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
-        .await
-        .unwrap();
-    let listener_ipv6 = tokio::net::TcpListener::bind(format!("::1:{port}"))
-        .await
-        .unwrap();
+    let port = env::var("PORT").expect("PORT must be set");
+    let listener_ipv4 = TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+    let listener_ipv6 = TcpListener::bind(format!("::1:{port}")).await.unwrap();
     println!("Listening on http://[::1]:{port} and http://0.0.0.0:{port} ...");
     tokio::select! {
         _ = axum::serve(listener_ipv4, app.clone()) => {},
@@ -47,10 +40,7 @@ struct RegisterRequest {
     email: String,
 }
 
-async fn register(
-    axum::extract::State(pool): axum::extract::State<MySqlPool>,
-    Json(req): Json<RegisterRequest>,
-) -> &'static str {
+async fn register(State(pool): State<MySqlPool>, Json(req): Json<RegisterRequest>) -> &'static str {
     let _ = sqlx::query("INSERT INTO registrations (email) VALUES (?)")
         .bind(req.email)
         .execute(&pool)
