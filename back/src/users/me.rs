45b59f1a99cbc8388a36d24abfa5b1302cd08797
Alejandro Alborno <233596652+alej-alborno@users.noreply.github.com>
2025-12-06 18:25:44 +0000

    back: add error type for user data endpoint

--- back/src/users/me.rs
+++ back/src/users/me.rs
@@ -2,15 +2,13 @@ use axum::{
     Json,
     extract::State,
     http::{HeaderMap, StatusCode},
-    response::IntoResponse,
+    response::{IntoResponse, Response},
 };
 use serde::Serialize;
 use sqlx::MySqlPool;
+use thiserror::Error;
 
-use crate::{
-    ApiResult,
-    users::auth::validate::{self, ValidationError},
-};
+use crate::{ApiResult, users::auth::validate};
 
 #[derive(utoipa::OpenApi)]
 #[openapi(paths(me))]
@@ -18,6 +16,24 @@ struct ApiDoc;
 pub fn openapi() -> utoipa::openapi::OpenApi {
     use utoipa::OpenApi;
     ApiDoc::openapi()
+}
+
+#[derive(Error, Debug)]
+pub enum UserDataError {
+    #[error("Could not query database")]
+    DatabaseError(#[from] sqlx::Error),
+}
+
+impl IntoResponse for UserDataError {
+    fn into_response(self) -> Response {
+        let status = match self {
+            Self::DatabaseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        let msg = self.to_string();
+
+        (status, Json(msg)).into_response()
+    }
 }
 
 #[derive(Serialize, sqlx::FromRow, utoipa::ToSchema)]
@@ -44,7 +60,7 @@ struct UserDataResponse {
 pub async fn me(State(pool): State<MySqlPool>, headers: HeaderMap) -> ApiResult<impl IntoResponse> {
     let session_token = validate::extract_session_token(headers)?;
 
-    match sqlx::query_as::<_, UserDataResponse>(
+    let user_data = sqlx::query_as::<_, UserDataResponse>(
         "SELECT accounts.id, accounts.email
             FROM sessions
             JOIN accounts ON accounts.id = sessions.account_id
@@ -52,11 +68,9 @@ pub async fn me(State(pool): State<MySqlPool>, headers: HeaderMap) -> ApiResult<
             LIMIT 1",
     )
     .bind(session_token)
-    .fetch_optional(&pool)
+    .fetch_one(&pool)
     .await
-    .map_err(ValidationError::DatabaseError)?
-    {
-        Some(v) => Ok((StatusCode::OK, Json(v))),
-        None => Err(ValidationError::InvalidToken.into()),
-    }
+    .map_err(UserDataError::DatabaseError)?;
+
+    Ok((StatusCode::OK, Json(user_data)))
 }
